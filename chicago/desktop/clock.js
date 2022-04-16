fe54482b5954a059158ee639b23529319fee2492
NYNEX SERVER 5 <65372786+CodeSalvageON@users.noreply.github.com>
2022-04-16 03:10:37 +0000

    Fix clock errors

--- chicago/desktop/clock.js
+++ chicago/desktop/clock.js
@@ -1,15 +1,31 @@
 const timedate = document.getElementById("timedate");
 
 function getTime (type) {
+  let fullDate = new Date();
+  
   switch (type) {
     case 0 :
-      let fullDate = new Date();
-
       return fullDate;
 
     case 1 :
-      let halfDate = new Date(hours, minutes);
+      let getWeirdDate = parseInt(fullDate.getHours() / 2);
+      let naturalDate = parseInt(fullDate.getHours());
 
+      if (naturalDate > 12) {
+        naturalDate = "PM";
+      }
+
+      else {
+        naturalDate = "AM";
+      }
+
+      let addZero = fullDate.getMinutes();
+
+      if (addZero.length < 2) {
+        addZero = "0" + String(addZero);
+      }
+      
+      let halfDate = getWeirdDate + ":" + addZero + " " + naturalDate;
       return halfDate;
   }
 }
