9c6ae9d4c0b643433004cc5a6e84beb576ac8f4b
NYNEX SERVER 5 <65372786+CodeSalvageON@users.noreply.github.com>
2022-04-16 04:03:55 +0000

    show and hide after clicking start button

--- chicago/desktop/start.js
+++ chicago/desktop/start.js
@@ -1,8 +1,18 @@
 const startButton = document.getElementById("start");
 const startMenu = document.getElementById("start-bar");
 
+let startFunc = 0;
+
 $("#start-bar").hide();
 
 $("#start").click(function () {
-  $("#start-bar").show();
+  if (startFunc === 0) {
+    $("#start-bar").show();
+    startFunc = 1;
+  }
+
+  else {
+    $("#start-bar").hide();
+    startFunc = 0;
+  }
 });
